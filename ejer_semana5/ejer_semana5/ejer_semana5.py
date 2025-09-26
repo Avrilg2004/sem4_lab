5b339fb956e468f4cc45e6a19cece55aff0a303a
diego <diegonicolasaguilarjimenez@gmail.com>
2025-09-26 09:04:31 -0500

    holi

--- ejer_semana5/ejer_semana5/ejer_semana5.py
+++ ejer_semana5/ejer_semana5/ejer_semana5.py
@@ -60,4 +60,33 @@ def ejer4():
             print("Área del circulo: ", round((math.pi * radio**2),2))
         case _: print("Opción invalida")
 
-ejer4()
+def ejer5():
+    n = int(input("Ingrese el valor de n: "))
+    suma =0
+    for i in range(1,n+1):
+        print(i)
+
+        if(i%2 == 0):
+            suma +=1
+    print("\nSuma de pares: ", suma)
+
+def ejer6():
+    cant = int(input("Ingrese la cantidad de numeros: "))
+    p = im = c = 0
+    print()
+    for i in range(1,cant+1):
+        num = int(input(f"Ingrese el numero {i}: "))
+
+        if num ==0:
+            c+=1
+        elif num %2 ==0:
+            p+=1
+        else: 
+            im+=1
+
+    print("\nCantidad de pares: ",p)
+    print("Cantidad de impares: ",im)
+    print("Cantidad de ceros: ",c)
+
+
+ejer6()
